df23a07636e04715e2b30996285c7ca5f20ed5fb
Noch <vasickinfedor@gmai.com>
2024-06-24 22:40:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function addNote() {
+    const notesList = document.getElementById('notes');
+    const newNoteInput = document.getElementById('newNote');
+    const newNoteText = newNoteInput.value.trim();
+
+    if (newNoteText !== "") {
+        const newNoteItem = document.createElement('li');
+        newNoteItem.innerHTML = `${newNoteText} <button onclick="editNote(this)">Редактировать</button> <button onclick="deleteNote(this)">Удалить</button>`;
+        notesList.appendChild(newNoteItem);
+        newNoteInput.value = "";
+    }
+}
+
+function editNote(button) {
+    const noteItem = button.parentElement;
+    const noteText = noteItem.firstChild.textContent.trim();
+    const newNoteText = prompt("Изменить заметку:", noteText);
+
+    if (newNoteText !== null && newNoteText.trim() !== "") {
+        noteItem.firstChild.textContent = newNoteText + " ";
+    }
+}
+
+function deleteNote(button) {
+    const noteItem = button.parentElement;
+    noteItem.remove();
+}
